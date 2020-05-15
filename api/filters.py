78a2e74d2505d6c74f773f2130aec67a2b547931
Дмитрий Брезгунов <dmitry.brezgunov@yandex.ru>
2020-05-15 16:09:11 +0300

    group project done

---
+++ api/filters.py
@@ -0,0 +1,13 @@
+from django_filters import rest_framework as filters
+
+from .models import Title
+
+
+class TitlesFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name='name', lookup_expr='contains')
+    genre = filters.CharFilter(field_name='genre__slug')
+    category = filters.CharFilter(field_name='category__slug')
+
+    class Meta:
+        model = Title
+        fields = ('name', 'category', 'genre', 'year')
